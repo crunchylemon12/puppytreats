b77590156ad3d7352f2060f4b2e0b0ed0c0e4200
crunchylemon12 <155703869+crunchylemon12@users.noreply.github.com>
2024-12-11 21:23:50 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDBsw7l5PeGd8OThfVoe-C4BfW1lxoVAD0",
+    authDomain: "treats-66c87.firebaseapp.com",
+    projectId: "treats-66c87",
+    storageBucket: "treats-66c87.firebasestorage.app",
+    messagingSenderId: "83367535274",
+    appId: "1:83367535274:web:35d0ea48925d82155666d3"
+};
